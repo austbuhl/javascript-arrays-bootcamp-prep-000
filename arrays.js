0b8662895cbf20b51c617af3cbe3f201cc149d2c
Austin Buhler <austinbuhler@gmail.com>
2020-05-07 19:07:03 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-  new_array = array.unshift(element);
-  return new_array
+  [element, ...array]
 }
